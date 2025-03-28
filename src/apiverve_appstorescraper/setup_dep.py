8cd3eba3a84f1fe3085cd6cb7f0eb7739ab6a14e
APIVerve Team <hello@apiverve.com>
2025-03-28 16:59:20 -0500

    Bug Fixes

--- src/apiverve_appstorescraper/setup_dep.py
+++ src/apiverve_appstorescraper/setup_dep.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='apiverve_appstorescraper',
-    version='1.1.6',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
